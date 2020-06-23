419dd900541e366c198cf913533b88b9cde11942
hurry <2539444133@qq.com>
2020-06-23 14:10:18 +0800

    modified start.c

--- start.c
+++ start.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-
+    
 int main(){
     int n,m = 0;
     scanf("%d",&n);
-    for(int i = 0; i < n; i++){
+    for(int i = 1; i <= n; i++){
          m+=i;
     }
     printf("%d",m);
